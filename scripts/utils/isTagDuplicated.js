42e74db3a3ce15fbfd63fde2219cf760f1dba5eb
Luis Garcia <luisarmandogarcia96@gmail.com>
2023-03-21 18:06:13 +0100

    Add tag css correction

---
+++ scripts/utils/isTagDuplicated.js
@@ -0,0 +1,7 @@
+/* 
+Create a function that verifies if the item selected 
+is already in the tag list. return true if so, false if not
+
+This function will be added as a middleware in the index.js file 
+when cliking on an item 
+*/
